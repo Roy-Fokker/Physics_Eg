12a8eef3f715a5e18cb03cb05bf48aa66c4db16d
Neel Raiyani <nraiyani@gmail.com>
2020-10-10 10:14:21 -0400

    update the pch file with more headers

--- src/pch.h
+++ src/pch.h
@@ -1,7 +1,16 @@
 #pragma once
 
 #include <Windows.h>
+
+#include <d3d11_4.h>
+#include <dxgi1_6.h>
+#include <atlbase.h>
+
 #include <string_view>
-#include <cstdint>
 #include <functional>
 #include <memory>
+#include <utility>
+#include <array>
+#include <vector>
+#include <cstdint>
+#include <cassert>
