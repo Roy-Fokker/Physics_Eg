a9fc956d200ca24e374dbf71952ec8429085600f
Neel Raiyani <nraiyani+github@gmail.com>
2020-11-29 12:06:46 -0500

    Create cube mesh and transform in main

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,44 @@
 #include "gfx/renderer.h"
 #include "sim/simulation.h"
 
+#include "gfx/gpu_data.h"
+
+namespace 
+{
+	auto make_cube_mesh()
+	{
+		return gfx::mesh
+		{
+			.vertices = std::vector<gfx::vertex>
+			{
+				{ { -1.0f, -1.0f, -1.0f }, { 0.0f, 0.0f, 0.0f, 1.0f } },
+				{ { -1.0f, +1.0f, -1.0f }, { 0.0f, 1.0f, 0.0f, 1.0f } },
+				{ { +1.0f, +1.0f, -1.0f }, { 1.0f, 1.0f, 0.0f, 1.0f } },
+				{ { +1.0f, -1.0f, -1.0f }, { 1.0f, 0.0f, 0.0f, 1.0f } },
+				{ { -1.0f, -1.0f, +1.0f }, { 0.0f, 0.0f, 1.0f, 1.0f } },
+				{ { -1.0f, +1.0f, +1.0f }, { 0.0f, 1.0f, 1.0f, 1.0f } },
+				{ { +1.0f, +1.0f, +1.0f }, { 1.0f, 1.0f, 1.0f, 1.0f } },
+				{ { +1.0f, -1.0f, +1.0f }, { 1.0f, 0.0f, 1.0f, 1.0f } },
+			},
+			.indicies = std::vector<uint32_t>
+			{
+				0, 1, 2, 0, 2, 3,
+				4, 6, 5, 4, 7, 6,
+				4, 5, 1, 4, 1, 0,
+				3, 2, 6, 3, 6, 7,
+				1, 5, 6, 1, 6, 2,
+				4, 0, 3, 4, 3, 7,
+			}
+		};
+	}
+}
+
 auto main() -> int
 {
+	// Data holders
 	bool quit{false};
+	auto cube_mesh = make_cube_mesh();
+	auto cube_matrix = gfx::matrix{};
 
 	auto wnd = os::window({
 		.title = L"Physics Example",
@@ -31,6 +66,8 @@ auto main() -> int
 	auto sim = sim::simulation();
 
 	auto clk = os::clock();
+	// Tell system about data
+	rndr.add_mesh(cube_mesh, cube_matrix);
 
 	wnd.show();
 	while (wnd.handle() and not quit)
